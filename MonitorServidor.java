8e298a67f61d4d3f45c739bf5120ea26babb43bd
tony030303 <antonio.sarmiento@est.fi.uncoma.edu.ar>
2024-08-22 10:56:08 -0300

    mecanismo de concurrencia ScheduledExecutorService

---
+++ MonitorServidor.java
@@ -0,0 +1,22 @@
+
+import java.util.Calendar;
+
+public class MonitorServidor implements Runnable {
+
+    private Servidor servidor;
+
+    public MonitorServidor(Servidor ser) {
+        this.servidor = ser;
+    }
+
+    @Override
+    public void run() {
+        if (this.servidor.getEstado()) {
+            System.out.println("Servidor funcionando correctamente" + " Tiempo Actual : "
+                    + Calendar.getInstance().get(Calendar.SECOND));
+        } else {
+            System.out.println("Servidor no responde" + " Tiempo Actual : "
+                    + Calendar.getInstance().get(Calendar.SECOND));
+        }
+    }
+}
---
+++ Servidor.java
@@ -0,0 +1,21 @@
+
+public class Servidor {
+
+    private boolean estadoFuncionando;
+
+    public Servidor() {
+        this.estadoFuncionando = true;
+    }
+
+    public synchronized void cambiarEstado() {
+        if (this.estadoFuncionando) {
+            this.estadoFuncionando = false;
+        } else {
+            this.estadoFuncionando = true;
+        }
+    }
+
+    public synchronized boolean getEstado() {
+        return this.estadoFuncionando;
+    }
+}
