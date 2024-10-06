c398ed6548973ee7850c90e8d57cb0e62646dba0
albanyPetit <albanydaniellepetit@gmail.com>
2024-10-06 18:13:10 -0300

    mejora barra nav

--- desarrollo web/Proyecto-Tienda/javascript/barraNav.js
+++ desarrollo web/Proyecto-Tienda/javascript/barraNav.js
@@ -2,20 +2,26 @@ const navBar = document.getElementById('navegador')
 
 
 //crear icono de ir a menu
-const logo = document.createElement('a');
-logo.className = 'logo';
-logo.href = '../HTML/index.html';
+const logo = document.createElement("a");
+logo.className = "logo";
+logo.href = "../HTML/index.html";
+
+const imagen = document.createElement("img");
+imagen.className = "imagenLogo";
+imagen.src = "../assets/01-logo.jpeg";
 
 //crear botones
 const navItems =
 [
-    {   class: 'boton',    href: '../HTML/indexcomida.html',   text: 'COMIDA'  },
+    {   class: 'boton',    href: '../HTML/indexComida.html',   text: 'COMIDA'  },
     {   class: 'boton',    href: '../HTML/indexComics.html',   text: 'COMICS'  },
     {   class: 'boton',    href: '../HTML/indexContacto.html', text: 'CONTACTO'}
 ];
 
 
 //insercion de menu
+
+logo.appendChild(imagen);
 navBar.appendChild(logo);
 
 //inserciones de botones
