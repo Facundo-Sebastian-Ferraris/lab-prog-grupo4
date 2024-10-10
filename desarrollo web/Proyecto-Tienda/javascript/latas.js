f36d30bfe8289a2e0bb59d6bb1097fdc4e78aea6
Facundo-Sebastian-Ferraris <facundo.ferraris@est.fi.uncoma.edu.ar>
2024-10-10 17:24:04 -0300

    creacionJson para latas

--- desarrollo web/Proyecto-Tienda/javascript/latas.js
+++ desarrollo web/Proyecto-Tienda/javascript/latas.js
@@ -1,33 +1,28 @@
 //DIV Contenedor
 
 const cartel = document.getElementById("cartel");
+let latas;
+fetch('../javascript/itemLatas.json')
+  .then(response => response.json())  // Convertir la respuesta a JSON
+  .then(data => {
+    latas = data;                // Asignar los datos a la variable
+    console.log(latas);              // Imprimir los datos en consola para verificar
 
-const items =
-[
-{class: "lata", style: "--etiqueta: url(../assets/PerkCans/01-perk.webp); --posicion:9"},
-{class: "lata", style: "--etiqueta: url(../assets/PerkCans/02-perk.webp); --posicion:7"},
-{class: "lata", style: "--etiqueta: url(../assets/PerkCans/03-perk.webp); --posicion:5"},
-{class: "lata", style: "--etiqueta: url(../assets/PerkCans/04-perk.webp); --posicion:3"},
-{class: "lata", style: "--etiqueta: url(../assets/PerkCans/05-perk.webp); --posicion:1"},
-{class: "lata", style: "--etiqueta: url(../assets/PerkCans/06-perk.webp); --posicion:-1"},
-{class: "lata", style: "--etiqueta: url(../assets/PerkCans/07-perk.webp); --posicion:-3"},
-{class: "lata", style: "--etiqueta: url(../assets/PerkCans/08-perk.webp); --posicion:-5"},
-{class: "lata", style: "--etiqueta: url(../assets/PerkCans/09-perk.webp); --posicion:-7"},
-{class: "lata", style: "--etiqueta: url(../assets/PerkCans/10-perk.webp); --posicion:-9"}
-];
+    latas.forEach(item => {
+        let div1 = document.createElement('div');
+        div1.className = "producto";
+    
+        let div2 = document.createElement('div');
+        div2.className = item.class;
+        //div2.style.cssText = item.style;
+        div2.style = item.style;
+        //div2.setAttribute("style",item.style);
+    
+        div1.appendChild(div2);
+    
+        cartel.appendChild(div1);
+    
+    });
+  })
+  .catch(error => console.error('Error cargando el archivo JSON:', error));
 
-items.forEach(item => {
-    let div1 = document.createElement('div');
-    div1.className = "producto";
-
-    let div2 = document.createElement('div');
-    div2.className = item.class;
-    //div2.style.cssText = item.style;
-    div2.style = item.style;
-    //div2.setAttribute("style",item.style);
-
-    div1.appendChild(div2);
-
-    cartel.appendChild(div1);
-
-});
