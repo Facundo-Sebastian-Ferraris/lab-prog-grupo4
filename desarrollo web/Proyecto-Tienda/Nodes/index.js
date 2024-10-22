5ddd7f01580c9f50edf53bfb15a7758b1fd69634
Facundo-Sebastian-Ferraris <facundo.ferraris@est.fi.uncoma.edu.ar>
2024-10-22 14:56:03 -0300

    implementacion oficial nodejs a geeknbites

---
+++ desarrollo web/Proyecto-Tienda/Nodes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../HTML')));
+app.use(express.static(path.join(__dirname, '../CSS')));
+app.use(express.static(path.join(__dirname, '../javascript')));
+app.use(express.static(path.join(__dirname, '../assets')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../HTML', 'index.html'));
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
